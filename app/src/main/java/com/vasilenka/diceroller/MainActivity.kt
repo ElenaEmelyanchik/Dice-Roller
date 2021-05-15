09c202e68df82517d7378e6509d1b7ae00f36c29
Vasilenka Lena <emelyanchik.elena@gmail.com>
2021-05-15 18:41:14 +0200

    Initial commit

---
+++ app/src/main/java/com/vasilenka/diceroller/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.vasilenka.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vasilenka.diceroller.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.buttonRoll.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        binding.imageViewRoll.setImageResource(rollImages.random())
+    }
+
+    private val rollImages = arrayOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+    )
+}
